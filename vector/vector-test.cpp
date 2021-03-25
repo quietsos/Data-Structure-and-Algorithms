946cf83ffdd1ad65719209fabc730eeff09a256b
Md Shohanuzzaman <shohaniu8@gmail.com>
2021-03-25 22:38:24 +0600

    iterator in vector

--- vector/vector-test.cpp
+++ vector/vector-test.cpp
@@ -2,6 +2,8 @@
 #include<stdio.h>
 #include<string>
 #include<vector>
+#include<iterator>
+
 
 
 using namespace std;
@@ -10,18 +12,26 @@ int main(){
 
     int array[3];
 
-    vector<int> vec(3,0); // this vector contain 3 element and all the element value will be 0
+  // vector<int> vec(3,0); // this vector contain 3 element and all the element value will be 0
+  vector<int> vec;
+   vector<int>::iterator it;
+
+   for(int i=1;i<=5;i++){
+        vec.push_back(i);
+   }
+//    vec.push_back(11);
 //    vec.push_back(34); //vec[0]=32
 //    vec.push_back(10); //vec[1]=10
 //    vec.push_back(11); //vec[3]=11
 
-    //cout<<vec;
-    printf("%d\n",vec[1]);
+//    printf("%d\n",vec[1]);
     cout<<vec.size()<<endl;
 
     for(int i=0;i<vec.size(); i++){
-        cout<<vec[i]<<endl;
+        cout<<vec[i]<<"\t";
     }
+    puts(""); //newline in vector
+
 
 
 
